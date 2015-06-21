98749e8768e9e06973295195230ea2fcab180537
Daniil Sydorenko <d.sydorenko@unit27.net>
2015-06-21 17:56:05 +0200

    Debris. Components. API. Bugs.

--- app/components/Shortener.php
+++ app/components/Shortener.php
@@ -17,44 +17,55 @@ use App\Entities\Url;
  *
  * @TODO Кирилица в описании
  * @TODO Дубликация ключей
- * @TODO CURL вместо header
  * @TODO Возврат descripion при помощи get_meta_tags
- * @TODO Возврат 0 или 1 или 2
- * @TODO Поправить валидацию
  * @TODO Сессия в Main и провер ка в JSON
- * @TODO выгеренирить desc in entities
- * @TODO
  *
  */
 class Shortener
 {
-
+    /**
+     * @param $url
+     * @return array
+     */
     public function shortenUrl($url)
     {
-        // Validate url
         $validUrl = $this->validateUrl($url);
-
-        if ($validUrl && $validUrl !== "debris") {
-
-            $Doctrine = Doctrine::getInstance();
-
-            $Url = $Doctrine->getRepository("App\\Models\\Url")->findOneBy(["url" => $url]);
-
-            $response = '';
-
-            // If url exists - send data to user
-            if ($Url instanceof \App\Entities\Url) {
+        switch ($validUrl) {
+            case 0 :
                 return $response = [
-                    'response' => $Url->getShortUrl(),
-                    'longUrl'  => $url,
-                    'urlViews' => $Url->getViews()
+                    'response' => "Your url bigger than 1000 signs. It is so strange, yeah?"
                 ];
-            } else {
-                // Set short url path
-                $rootPath = 'http://'. \getenv('HTTP_HOST') . \dirname(\getenv('SCRIPT_NAME'));
-                $shortUrl = $rootPath . \substr(md5(uniqid(rand(),1)),0,4);
+                break;
 
-                // If key is duplicated - generating new key till will find the original one
+            case 1 :
+                return $response = [
+                    'response' => "We are so sorry, but you url is invalid."
+                ];
+                break;
+
+            case 2 :
+                return $response = [
+                'response' => "Wazzup, this is a Debris shortened url."
+            ];
+                break;
+
+            case 3 :
+                $urlFiltered = (\mb_substr($url, -1) == '/') ? \mb_strtolower($url) : \mb_strtolower($url) . '/';
+                $Doctrine = Doctrine::getInstance();
+                $Url = $Doctrine->getRepository("App\\Models\\Url")->findOneBy(["url" => $urlFiltered]);
+                $response = '';
+                if ($Url instanceof \App\Entities\Url) {
+                    return $response = [
+                        'response' => $Url->getShortUrl(),
+                        'longUrl'  => $url,
+                        'urlViews' => $Url->getViews()
+                    ];
+                } else {
+                    // Set short url path
+                    $rootPath = 'http://'. \getenv('HTTP_HOST') . \dirname(\getenv('SCRIPT_NAME'));
+                    $shortUrl = $rootPath . \substr(md5(uniqid(rand(),1)),0,4);
+
+                    // If key is duplicated - generating new key till will find the original one
 //                do {
 //                    $duplicatedUrlKey = $this->duplicatedUrlKey($shortUrl, $rootPath);
 //                    if (!$duplicatedUrlKey) {
@@ -62,47 +73,37 @@ class Shortener
 //                    }
 //                } while ($duplicatedUrlKey);
 
-                // Set hash(for future, maybe a password ??)
-                $hash = \sha1(\md5(\uniqid()));
+                    // Set hash(for future, maybe a password ??)
+                    $hash = \sha1(\md5(\uniqid()));
 
-                // Try to get the client ip address
-                $userIp = '';
+                    $userIp = '';
+                    if (\getenv('HTTP_CLIENT_IP'))
+                        $userIp = getenv('HTTP_CLIENT_IP');
+                    else if(\getenv('HTTP_X_FORWARDED_FOR'))
+                        $userIp = getenv('HTTP_X_FORWARDED_FOR');
+                    else if(\getenv('HTTP_X_FORWARDED'))
+                        $userIp = getenv('HTTP_X_FORWARDED');
+                    else if(\getenv('HTTP_FORWARDED_FOR'))
+                        $userIp = getenv('HTTP_FORWARDED_FOR');
+                    else if(\getenv('HTTP_FORWARDED'))
+                        $userIp = getenv('HTTP_FORWARDED');
+                    else if(\getenv('REMOTE_ADDR'))
+                        $userIp = getenv('REMOTE_ADDR');
+                    else
+                        $userIp = 'UNKNOWN';
 
-                if (\getenv('HTTP_CLIENT_IP'))
-                    $userIp = getenv('HTTP_CLIENT_IP');
-                else if(\getenv('HTTP_X_FORWARDED_FOR'))
-                    $userIp = getenv('HTTP_X_FORWARDED_FOR');
-                else if(\getenv('HTTP_X_FORWARDED'))
-                    $userIp = getenv('HTTP_X_FORWARDED');
-                else if(\getenv('HTTP_FORWARDED_FOR'))
-                    $userIp = getenv('HTTP_FORWARDED_FOR');
-                else if(\getenv('HTTP_FORWARDED'))
-                    $userIp = getenv('HTTP_FORWARDED');
-                else if(\getenv('REMOTE_ADDR'))
-                    $userIp = getenv('REMOTE_ADDR');
-                else
-                    $userIp = 'UNKNOWN';
+                    // Try to save
+                    $result = $this->setShortUrl($url, $shortUrl, $hash, $userIp);
+                    $Url = $Doctrine->getRepository("App\\Models\\Url")->findOneBy(["url" => $url]);
+                    $responseResult = (!empty($result)) ? $result : $shortUrl;
 
-                // Try to save
-                $result = $this->setShortUrl($url, $shortUrl, $hash, $userIp);
-                $Url = $Doctrine->getRepository("App\\Models\\Url")->findOneBy(["url" => $url]);
-                $responseResult = (!empty($result)) ? $result : $shortUrl;
-
-                return $response = [
-                    'response' => $responseResult,
-                    'longUrl'  => $url,
-                    'urlViews' => 0
-                ];
-
-            }
-        } else if ($validUrl && $validUrl === "debris") {
-            return $response = [
-                'response' => "Wazzup, this is a Debris shortened URL :)"
-            ];
-        } else {
-            return $response = [
-                'response' => "We are so sorry, but you URL is invalid!"
-            ];
+                    return $response = [
+                        'response' => $responseResult,
+                        'longUrl'  => $url,
+                        'urlViews' => 0
+                    ];
+                }
+                break;
         }
     }
 
@@ -134,50 +135,35 @@ class Shortener
      */
     protected function validateUrl($url)
     {
-        /* И здесь тоже проверять длину !
-         *! to lowercase
-         * ! rtrim - "/" - чтобы со слешо и без слеша в конце - было одно и тоже
-         *
-         *
-         * 1. Check by pregmatch, if not url - send 0
-         * - If yes :
-         *  2. If "debris url" - send 1
-                If no:
-         *      3. Send request, if didnt find "200" - send 0
-         *          - if yes - send 2
-         * */
+        if (\mb_strlen($url, 'UTF-8') < 1000) {
+            if (\preg_match('/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i', $url)) {
 
-        if (\preg_match('/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i', $url)) {
-            // If was shortered by Debris
-            $strinurl = \strpos($url, "debris.dev");
+                //@TODO А если запиздолят просто слово debris в строке ??
+                $urlDebris = \strpos(\mb_strtolower($url), "debris");
 
-            if ($strinurl) {
-                return "debris";
+                if ($urlDebris) {
+                    return 2;
+                } else {
+                    $agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_USERAGENT,      $agent);
+                    curl_setopt($ch, CURLOPT_URL,            $url);
+                    curl_setopt($ch, CURLOPT_HEADER,         true);
+                    curl_setopt($ch, CURLOPT_NOBODY,         true);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($ch, CURLOPT_TIMEOUT,        15);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+                    $document = curl_exec($ch);
+                    $document = explode("\n", $document);
+
+                    return (\strpos($document[0],"200")) ? 3 : 0;
+                }
             } else {
-                // User agent(Browser)
-                $agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
-
-                // Initialize curl
-                $ch = curl_init();
-
-                curl_setopt($ch, CURLOPT_USERAGENT,      $agent);
-                curl_setopt($ch, CURLOPT_URL,            $url);
-                curl_setopt($ch, CURLOPT_HEADER,         true);
-                curl_setopt($ch, CURLOPT_NOBODY,         true);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_TIMEOUT,        15);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-
-                // Execute curl
-                $document = curl_exec($ch);
-                $document = explode("\n", $document);
-
-                // Return true if found 200 in the response
-                return (\strpos($document[0],"200")) ? true : false;
+                return 1;
             }
         } else {
-            return false;
+            return 0;
         }
     }
 
@@ -192,7 +178,7 @@ class Shortener
      */
     protected function setShortUrl($url, $shortUrl, $hash, $userIp)
     {
-        // Create new URL
+        // Create new url
         $Url = new Url();
         $Url->setUrl($url);
         $Url->setShortUrl($shortUrl);
@@ -200,11 +186,7 @@ class Shortener
         $Url->setViews(0);
         $Url->setHash($hash);
         $Url->setIp($userIp);
-
         $Doctrine = Doctrine::getInstance();
-
-
-        // Save to db
         try {
             $Doctrine->persist($Url);
             $Doctrine->flush();
--- app/controllers/API/Shortener.php
+++ app/controllers/API/Shortener.php
@@ -9,6 +9,7 @@
 // Namespace
 namespace App\API;
 
+use App\Components\Shortener as ShortenerComponent;
 
 /**
  * Shortener class
@@ -18,15 +19,200 @@ namespace App\API;
  * @TODO
  * @TODO
  */
-class Shortener extends API
+class Shortener extends \Gaia\Controllers\JSON
 {
-    public function short($url) {
-        // lOGIC
-    }
+    /**
+     * Property: method
+     * The HTTP method this request was made in, either GET, POST, PUT or DELETE
+     */
+    private $method = null;
 
-    public function checkRequest()
+    /**
+     * Property: endpoint
+     * The Model requested in the URI. eg: /files
+     */
+    private $endpoint = null;
+
+    /**
+     * Property: args
+     * Any additional URI components after the endpoint and verb have been removed, in our
+     * case, an integer ID for the resource. eg: /<endpoint>/<verb>/<arg0>/<arg1>
+     * or /<endpoint>/<arg0>
+     */
+    private $args = [];
+
+    /**
+     * Property: $request
+     *
+     */
+    private $request = null;
+
+    /**
+     * @return mixed
+     */
+    public function getMethod()
     {
-
+        return $this->method;
     }
 
+    /**
+     * @param mixed $method
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * Method: endpoint
+     * The Model requested in the URI. eg: /files
+     */
+    public function setEndpoint()
+    {
+        $endpoint = $this->getRequest()->getURI();
+        if (!empty($endpoint)) {
+            $this->endpoint = explode('', \rtrim($endpoint), '/');
+        }
+    }
+
+    /**
+     * Method: endpoint
+     * The Model requested in the URI. eg: /files
+     */
+    public function getEndpoint()
+    {
+        return $this->endpoint[2];
+    }
+
+    /**
+     * Method: setArgs
+     * Set array with arguments from request
+     * @param array $parameters
+     */
+    public function setArgs(array $parameters)
+    {
+        $this->args = $parameters;
+    }
+
+    /**
+     * Method: getArgs
+     * Return array with arguments from request
+     */
+    public function getArgs()
+    {
+        return $this->args;
+    }
+
+    /**
+     * Method: short
+     * Return shortened urls
+     * @param string $url
+     */
+
+    /**
+     * Method: checkRequest
+     * Allow for CORS, assemble and pre-process the data
+     */
+    public function short()
+    {
+        header("Access-Control-Allow-Orgin: *");
+        header("Access-Control-Allow-Methods: *");
+        header("Content-Type: application/json");
+
+        $this->setMethod($_SERVER['REQUEST_METHOD']);
+        $this->setArgs((array)$this->getRequest()->getParameters());
+
+        switch ($this->getMethod()) {
+        case 'GET':
+                $endpoint = \strtolower($this->getEndpoint());
+
+                if (empty($endpoint) && $endpoint == "short") {
+                    $this->_response('Invalid Path', 400);
+                } else {
+                    $requestData = $this->request = $this->_cleanInputs($this->getArgs());
+                    $this->_response('OK', 200);
+
+                    // Защитить от написания лишнего
+                    // или нправильного пути
+                    // endpoint short
+                    // или нправильного пути
+                    // или нправильного пути
+
+
+//                    return $response = [
+//                        'urlViews' => $requestData['url']
+//                    ];
+
+                    $ShortenerComponent = new ShortenerComponent();
+                    return $ShortenerComponent->shortenUrl($requestData['url']);
+                }
+            break;
+        case 'DELETE':
+        case 'POST':
+        case 'PUT':
+            $this->_response('Invalid Method', 405);
+            break;
+        default:
+            $this->_response('Invalid Method', 405);
+            break;
+        }
+    }
+
+    /**
+     * Method: short
+     * Allow for CORS, assemble and pre-process the data
+     */
+    public function processAPI()
+    {
+        if (method_exists($this, $this->endpoint)) {
+            return $this->_response($this->{$this->endpoint}($this->args));
+        }
+        return $this->_response("No Endpoint: $this->endpoint", 404);
+    }
+
+    /**
+     * Method: _response
+     *
+     */
+    private function _response($data, $status = 200)
+    {
+        header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
+        return json_encode($data);
+    }
+
+    /**
+     * Method: _cleanInputs
+     * Clean data
+     * @param mixed $data
+     * @return array|string
+     */
+    private function _cleanInputs($data)
+    {
+        $clean_input = [];
+        if (is_array($data)) {
+            foreach ($data as $k => $v) {
+                $clean_input[$k] = $this->_cleanInputs($v);
+            }
+        } else {
+            $clean_input = trim(strip_tags($data));
+        }
+        return $clean_input;
+    }
+
+    /**
+     * Method: _requestStatus
+     *
+     * @param integer $code
+     */
+    private function _requestStatus($code)
+    {
+        $status = [
+            200 => 'OK',
+            400 => 'Bad Request',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            500 => 'Internal Server Error',
+        ];
+        return ($status[$code]) ? $status[$code] : $status[500];
+    }
 }
